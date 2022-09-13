0cb2e1e1ffed4ddc14f0d9ab0807ce9c53606e7a
João Pedro <jpcabana1@gmail.com>
2022-09-13 00:13:49 -0300

    Work Queues

--- Work Queues/Worker/Worker.cs
+++ Work Queues/Worker/Worker.cs
@@ -6,21 +6,34 @@ var factory = new ConnectionFactory() { HostName = "localhost" };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
-    channel.QueueDeclare(queue: "hello",
-                         durable: false,
+    channel.QueueDeclare(queue: "task_queue",
+                         durable: true,
                          exclusive: false,
                          autoDelete: false,
                          arguments: null);
 
+    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+    Console.WriteLine(" [*] Waiting for messages.");
+
     var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
+    consumer.Received += (sender, ea) =>
     {
         var body = ea.Body.ToArray();
         var message = Encoding.UTF8.GetString(body);
         Console.WriteLine(" [x] Received {0}", message);
+
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        Console.WriteLine(" [x] Done");
+
+        // Note: it is possible to access the channel via
+        //       ((EventingBasicConsumer)sender).Model here
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
     };
-    channel.BasicConsume(queue: "hello",
-                         autoAck: true,
+    channel.BasicConsume(queue: "task_queue",
+                         autoAck: false,
                          consumer: consumer);
 
     Console.WriteLine(" Press [enter] to exit.");
